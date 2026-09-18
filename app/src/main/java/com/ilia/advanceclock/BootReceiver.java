db52ae9856d379871f4ecab7ecb808cc6c593575
ilia55555 <118246388+ilia55555@users.noreply.github.com>
2026-09-18 14:50:27 +0330

    Add BootReceiver.java

---
+++ app/src/main/java/com/ilia/advanceclock/BootReceiver.java
@@ -0,0 +1,11 @@
+package com.ilia.advanceclock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public final class BootReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {
+        AlarmScheduler.rescheduleAll(context);
+    }
+}
